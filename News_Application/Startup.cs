9d0c174f7ee20cba449f467399b9dc05a3c7dcdd
mustafa magdy <mustafa.magdy1111@gmail.com>
2019-01-21 01:23:49 -1200

    Add project files.

---
+++ News_Application/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(News_Application.Startup))]
+namespace News_Application
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
